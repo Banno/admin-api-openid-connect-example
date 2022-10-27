d1753c36aed954ccfebd209438a6b512950711e4
Sam Couch <sam.couch@banno.com>
2022-10-27 09:54:13 -0400

    typo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     auth_wellknown: 'https://banno.com/a/oidc-provider/api/v0/.well-known/openid-configuration',
     client: {
         client_id: '', // Your Client ID
-        client_secret: '', // Yourt Client Secret
+        client_secret: '', // Your Client Secret
         response_types: ['code'],
         redirect_uris: ['https://localhost:8080/auth/cb']
     }
